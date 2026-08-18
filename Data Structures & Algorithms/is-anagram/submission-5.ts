bd6d9def08c40cc8c68683cfad2673fecb9ae537
Damien <damienksedgwick@gmail.com>
2026-08-18 11:26:54 +0100

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.ts
@@ -0,0 +1,15 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s: string, t: string): boolean {
+        if (s.length !== t.length) return false;
+
+        const s2 = s.split('').sort().join('');
+        const t2 = t.split('').sort().join('');
+
+        return s2 === t2;
+    }
+}
